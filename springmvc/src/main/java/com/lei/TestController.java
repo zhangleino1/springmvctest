d6e21f66adaf2bebd0417809c462e8571166b378
zhanglei <851106500@qq.com>
2016-06-12 14:23:23 +0800

    初次导入

---
+++ springmvc/src/main/java/com/lei/TestController.java
@@ -0,0 +1,29 @@
+package com.lei;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by raymon on 2016/6/8.
+ */
+@Controller
+public class TestController {
+    @RequestMapping(value="/test")
+    @ResponseBody
+    public Map<String,String> test(HttpServletRequest request){
+        Map<String,String> map = new HashMap<String, String>();
+        map.put("sfzh",request.getParameter("sfzh"));
+        return map;
+    }
+    @RequestMapping(value="/cs")
+    public String index(){
+        return  "def";
+    }
+}
